a2e90a94ca55e9e5bcb0cf18c8849d79d3eaabbf
Valerie Ludden <valudden@gmail.com>
2020-05-02 17:53:28 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<?php $project_link = "Projects Page";?>
+<a href="projects.php"><?php echo "Go to ".$project_link ?></a>
+
+</body>
+</html>
---
+++ projects.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Projects Page</title>
+    <link href="assets/styles.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<h1>Under construction....more projects coming soon</h1>
+
+</body>
+</html>

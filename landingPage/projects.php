c5cfeaf4111589fa4fa86ca11aade697888a46e0
Valerie Ludden <valudden@gmail.com>
2020-05-07 19:46:44 -0600

    merge4

--- landingPage/projects.php
+++ landingPage/projects.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Projects Page</title>
-    <link href="assets/styles.css" rel="stylesheet" type="text/css"/>
+    <link href="landingPage/assets/styles.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
     <h1>Under construction....more projects coming soon</h1>

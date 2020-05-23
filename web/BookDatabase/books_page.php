f43c428eda45ee58c2aae328a572dd5b0d3a0cf3
Valerie Ludden <valudden@gmail.com>
2020-05-22 23:29:44 -0600

    testing db query

--- web/BookDatabase/books_page.php
+++ web/BookDatabase/books_page.php
@@ -7,15 +7,16 @@ $book = $_POST["book"];
 
 echo "THE book you picked was - " . $book;
 
-foreach ($db->query('SELECT title, author FROM book WHERE title ='. $book) as $row)
+foreach ($db->query('SELECT title, author FROM book WHERE book = "' . $book . '"') as $row)
+//foreach ($db->query('SELECT title, author FROM book WHERE title = "$book"') as $row)
 //foreach ($db->query('SELECT b.title, a.name FROM book b, author a WHERE b.author = a.id AND b.location = l.id') as $row)
 //foreach ($db->query('SELECT  b.title, a.name,  FROM book b, author a WHERE b.author = a.id') as $row)
 {
     echo '<p>';
     echo '<b>';
     echo $row['title'];
-    echo ' : ';
-    echo $row['name'];
+    echo ' : author id = ';
+    echo $row['author'];
     echo '</b>';
     echo '<p/>';
 }

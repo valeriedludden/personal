03bd88ca24a949b08c058795df151b28e6e0a44b
Valerie Ludden <valudden@gmail.com>
2020-05-22 21:15:15 -0600

    dab hook up 5

--- web/BookDatabase/index_library.php
+++ web/BookDatabase/index_library.php
@@ -2,22 +2,9 @@
 include "header.php";
 require "dbConnect.php";
 
-//$statement = $db->query('SELECT username, password FROM note_user');
-//$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
 $db = get_db();
-//$dbArray = $db->query('SELECT title, location, author FROM book');
-//$results = $dbArray->fetchAll(PDO::FETCH_ASSOC);
-//foreach ($results as $row)
-//{
-//    echo 'Title - ' . $row['title'] . ' ';
-//    echo 'Author - ' . $row['author'] . ' ';
-//    echo 'Location - ' . $row['location'] . '';
-//    echo '<br/>';
-//}
 
-foreach ($db->query('SELECT title, location, author FROM book') as $row)
+foreach ($db->query('SELECT b.title, l.location, a.name FROM book b, location l, author a WHERE b.author = a.id AND b.location') as $row)
 {
     echo 'Title - ' . $row['title'] . ' ';
     echo 'Author - ' . $row['author'] . ' ';
@@ -28,7 +15,7 @@ foreach ($db->query('SELECT title, location, author FROM book') as $row)
 <body>
 
 <div class="container">
-    <form method="POST">
+    <form method="POST" action="">
         <label for="book">Book:</label>
         <input type="text" id="book" name="book"><br />
         <input type="submit" value="Submit"> <br />

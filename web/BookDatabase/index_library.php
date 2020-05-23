b5af54781fadf08d5277b52ca5f5c6f549d029f0
Valerie Ludden <valudden@gmail.com>
2020-05-22 19:12:15 -0600

    dab hook up 4

--- web/BookDatabase/index_library.php
+++ web/BookDatabase/index_library.php
@@ -1,37 +1,37 @@
 <?php
 include "header.php";
-//try
-//{
-//    $dbUrl = getenv('DATABASE_URL');
-//
-//    $dbOpts = parse_url($dbUrl);
-//
-//    $dbHost = $dbOpts["host"];
-//    $dbPort = $dbOpts["port"];
-//    $dbUser = $dbOpts["user"];
-//    $dbPassword = $dbOpts["pass"];
-//    $dbName = ltrim($dbOpts["path"],'/');
-//
-//    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-//
-//    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//}
-//catch (PDOException $ex)
-//{
-//    echo 'Error!: ' . $ex->getMessage();
-//    die();
-//}
-//# You SHOULD do this:
-//$dbUrl = getenv('DATABASE_URL');
-//# You SHOULD do this:
-//$dbUser = $dbOpts["user"];
-//foreach ($db->query('SELECT book, location, author') as $row)
-//{
-//    echo 'book' . $row['book'] . ' ';
-//    echo 'author' . $row['author'] . ' ';
-//    echo 'location' . $row['location'] . '';
-//    echo '<br/>';
-//}
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+# You SHOULD do this:
+$dbUrl = getenv('DATABASE_URL');
+# You SHOULD do this:
+$dbUser = $dbOpts["user"];
+foreach ($db->query('SELECT title, location, author FROM book') as $row)
+{
+    echo 'book' . $row['book'] . ' ';
+    echo 'author' . $row['author'] . ' ';
+    echo 'location' . $row['location'] . '';
+    echo '<br/>';
+}
 ?>
 <body>
 

41e6b0579f71693126baa0126918f153ceabb882
Valerie Ludden <valudden@gmail.com>
2020-05-22 19:10:07 -0600

    dab hook up 3

--- web/BookDatabase/index_library.php
+++ web/BookDatabase/index_library.php
@@ -1,45 +1,46 @@
 <?php
 include "header.php";
-try
-{
-    $dbUrl = getenv('DATABASE_URL');
-
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-}
-# You SHOULD do this:
-$dbUrl = getenv('DATABASE_URL');
-# You SHOULD do this:
-$dbUser = $dbOpts["user"];
-foreach ($db->query('SELECT book, location, author') as $row)
-{
-    echo 'book' . $row['book'] . ' ';
-    echo 'author' . $row['author'] . ' ';
-    echo 'location' . $row['location'] . '';
-    echo '<br/>';
-}
+//try
+//{
+//    $dbUrl = getenv('DATABASE_URL');
+//
+//    $dbOpts = parse_url($dbUrl);
+//
+//    $dbHost = $dbOpts["host"];
+//    $dbPort = $dbOpts["port"];
+//    $dbUser = $dbOpts["user"];
+//    $dbPassword = $dbOpts["pass"];
+//    $dbName = ltrim($dbOpts["path"],'/');
+//
+//    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//
+//    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//}
+//catch (PDOException $ex)
+//{
+//    echo 'Error!: ' . $ex->getMessage();
+//    die();
+//}
+//# You SHOULD do this:
+//$dbUrl = getenv('DATABASE_URL');
+//# You SHOULD do this:
+//$dbUser = $dbOpts["user"];
+//foreach ($db->query('SELECT book, location, author') as $row)
+//{
+//    echo 'book' . $row['book'] . ' ';
+//    echo 'author' . $row['author'] . ' ';
+//    echo 'location' . $row['location'] . '';
+//    echo '<br/>';
+//}
 ?>
 <body>
 
 <div class="container">
-    <form method="POST" action="books_page.php"> <label for="book">Book:</label>
+    <form method="POST">
+        <label for="book">Book:</label>
         <input type="text" id="book" name="book"><br />
         <input type="submit" value="Submit"> <br />
-    </form>'t type="submit" value="Search for Book " name="bookSearch">
+    </form>
     </form>
 
 </div>

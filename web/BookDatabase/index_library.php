5227037259bc4e7b84914d66e877d1749849feb1
Valerie Ludden <valudden@gmail.com>
2020-05-22 20:19:31 -0600

    dab hook up 5

--- web/BookDatabase/index_library.php
+++ web/BookDatabase/index_library.php
@@ -1,35 +1,12 @@
 <?php
 include "header.php";
-try
-{
-    $dbUrl = getenv('DATABASE_URL');
-
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-}
-# You SHOULD do this:
-$dbUrl = getenv('DATABASE_URL');
-# You SHOULD do this:
-$dbUser = $dbOpts["user"];
+require "dbConnect.php";
+$db = get_db();
 foreach ($db->query('SELECT title, location, author FROM book') as $row)
 {
-    echo 'book' . $row['book'] . ' ';
-    echo 'author' . $row['author'] . ' ';
-    echo 'location' . $row['location'] . '';
+    echo 'Title - ' . $row['title'] . ' ';
+    echo 'Author - ' . $row['author'] . ' ';
+    echo 'Location - ' . $row['location'] . '';
     echo '<br/>';
 }
 ?>

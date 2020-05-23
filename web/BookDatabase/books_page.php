0926a6bc92892d749792f1a9a028e9a093eb8509
Valerie Ludden <valudden@gmail.com>
2020-05-22 22:35:31 -0600

    dab hook up 5

--- web/BookDatabase/books_page.php
+++ web/BookDatabase/books_page.php
@@ -1,6 +1,20 @@
 <?php
 include "header.php";
 echo "Books Page";
+require "dbConnect.php";
+$db = get_db();
+$book = $_POST["book"];
+
+foreach ($db->query('SELECT  b.title, a.name,  FROM book b, author a WHERE b.title = "' . $book . '"') as $row)
+{
+    echo '<p>';
+    echo '<b>';
+    echo $row['title'];
+    echo ' : ';
+    echo $row['name'];
+    echo '</b>';
+    echo '<p/>';
+}
 ?>
 <div class="container">
 

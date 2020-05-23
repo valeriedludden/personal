d16dfc91fe21f8bdaab5e2c3006ffedcf16957ed
Valerie Ludden <valudden@gmail.com>
2020-05-23 14:22:25 -0600

    test2

--- web/BookDatabase/books_page.php
+++ web/BookDatabase/books_page.php
@@ -5,7 +5,7 @@ require "dbConnect.php";
 $db = get_db();
 
 $book = $_POST["book"];
-$dbbq = "SELECT b.title, a.name FROM book b, author a WHERE title ='$book' AND b.author = a.id";
+$dbbq = "SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE title ='$book' AND b.author = a.id AND b.location = l.id AND b.genre = g.id";
 
 
 foreach ($db->query($dbbq) as $row)
@@ -15,11 +15,18 @@ foreach ($db->query($dbbq) as $row)
     echo $row['title'];
     echo ' : author = ';
     echo $row['name'];
+    echo ' : author = ';
+    echo $row['location'];
+    echo ' : location = ';
+    echo $row['genre'];
+    echo ' : genre = ';
+
     echo '</b>';
     echo '<p/>';
 }
 
-?>
-<div class="container">
 
-</div>
+echo '<div class="container">
+
+</div>'
+?>

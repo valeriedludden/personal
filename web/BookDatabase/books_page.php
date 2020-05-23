4fe687a3f099b529760240bbcf523eec9691f821
Valerie Ludden <valudden@gmail.com>
2020-05-23 08:55:16 -0600

    test2

--- web/BookDatabase/books_page.php
+++ web/BookDatabase/books_page.php
@@ -3,22 +3,12 @@ include "header.php";
 echo "Books Page";
 require "dbConnect.php";
 $db = get_db();
-$book1 = 'EMMA';
+
 $book = $_POST["book"];
-//$dbq = "SELECT title, author FROM book";
-$dbq = "SELECT b.title, a.name FROM book b, author a WHERE title ='$book' AND b.author = a.id";
-$dbq1 = "SELECT title, author FROM book WHERE title = '$book'";
-$dbq2 = "SELECT title, author FROM book WHERE title = '$book1'";
+$dbbq = "SELECT b.title, a.name FROM book b, author a WHERE title ='$book' AND b.author = a.id";
 
-echo "THE book you picked was - " . $book;
-//$bookQuery = 'SELECT title, author FROM book WHERE title=';
 
-//foreach ($db->query('SELECT title, author FROM book') as $row)
-foreach ($db->query($dbq) as $row)
-//foreach ($db->query('SELECT title, author FROM book WHERE title ="$book"' ) as $row)
-//foreach ($db->query($bookQuery . $book) as $row)
-//foreach ($db->query('SELECT b.title, a.name FROM book b, author a WHERE b.author = a.id AND b.location = l.id') as $row)
-//foreach ($db->query('SELECT  b.title, a.name,  FROM book b, author a WHERE b.author = a.id') as $row)
+foreach ($db->query($dbbq) as $row)
 {
     echo '<p>';
     echo '<b>';
@@ -31,32 +21,6 @@ foreach ($db->query($dbq) as $row)
     echo '<p/>';
 }
 
-foreach ($db->query($dbq1) as $row)
-{
-    echo '<p>';
-    echo '<b>';
-    echo $row['title'];
-    echo ' : author id = ';
-    echo $row['author'];
-    echo '</b>';
-    echo "the second one";
-    echo '</b>';
-    echo '<p/>';
-}
-
-
-foreach ($db->query($dbq2) as $row)
-{
-echo '<p>';
-    echo '<b>';
-        echo $row['title'];
-        echo ' : author id = ';
-        echo $row['author'];
-        echo '</b>';
-    echo "the third one";
-    echo '</b>';
-    echo '<p/>';
-}
 ?>
 <div class="container">
 

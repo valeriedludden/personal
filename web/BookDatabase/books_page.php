aacb712318f370ed5f92af4c089c32dae4cc9a00
Valerie Ludden <valudden@gmail.com>
2020-05-23 23:41:30 -0600

    test2

--- web/BookDatabase/books_page.php
+++ web/BookDatabase/books_page.php
@@ -4,7 +4,7 @@ include "header.php";
 require "dbConnect.php";
 $db = get_db();
 
-$book = strtoupper($_POST["book"]);
+$book = strtoupper(htmlspecialchars($_POST["book"]));
 $statement = $db->query("SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE title ='$book' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
@@ -42,3 +42,5 @@ echo ''
 <div class="container">
 
 </div>
+</body>
+</html>

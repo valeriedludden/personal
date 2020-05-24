d56bd9ff8aa575a1eca8c8bf13404b8b115a1151
Valerie Ludden <valudden@gmail.com>
2020-05-24 00:08:31 -0600

    test2

--- web/BookDatabase/location.php
+++ web/BookDatabase/location.php
@@ -7,8 +7,9 @@ $location = $_POST["location"];
 //$locationName = $db->query("SELECT l.location FROM location l WHERE l.id = $location");
 $statement = $db->query("SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE l.id ='$location' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$locs = ['Den Bookcase Left','Den Bookcase Right', 'Barrister Bookcase West', 'Barrister Bookcase East', 'Guest Bedroom Shelves', 'STE Michelle Shopping Bag'];
 
-echo "<h1>Below is a list of books located in ".$results['l.location'] ." </h1></br>";
+echo "<h1>Below is a list of books located in $locs[$location]</h1></br>";
 echo "<b><div class='container'>
 <ul class='list-group list-group-horizontal'>
         <li class='list-group-item g-one'>Title</li>

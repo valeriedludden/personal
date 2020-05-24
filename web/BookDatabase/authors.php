d92f84e02b9995b46ffcede702831bd5358d25a7
Valerie Ludden <valudden@gmail.com>
2020-05-23 21:12:57 -0600

    test2

--- web/BookDatabase/authors.php
+++ web/BookDatabase/authors.php
@@ -1,7 +1,39 @@
 <?php
 include "header.php";
 echo "Authors Page";
-?>
-<div class="container">
+require "dbConnect.php";
+$db = get_db();
 
-</div>
+$author = strtoupper($_POST["author"]);
+$dbbq = "SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE a.name ='$author' AND b.author = a.id AND b.location = l.id AND b.genre = g.id";
+foreach ($db->query($dbbq) as $row)
+{
+    echo '<div class="container">
+    <ul class="list-group list-group-horizontal">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Morbi leo risus</li>
+    </ul>
+</div>';
+    echo '<b>';
+    echo $row['title'];
+    echo ' : author = ';
+    echo $row['name'];
+    echo ' : author = ';
+    echo $row['location'];
+    echo ' : location = ';
+    echo $row['genre'];
+    echo ' : genre = ';
+
+    echo '</b>';
+    echo '<p/>';
+}
+echo '<div class="container">
+    <ul class="list-group list-group-horizontal">
+        <li class="list-group-item">Bottop</li>
+        <li class="list-group-item">zushduvhzdvxc</li>
+        <li class="list-group-item">jkhdksfgksjdfhkjsdfks</li>
+    </ul>
+</div>';
+?>
+

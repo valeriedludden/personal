277773c13ffa318508e402bbf252de27dacee9a0
Valerie Ludden <valudden@gmail.com>
2020-05-23 22:56:48 -0600

    test2

--- web/BookDatabase/location.php
+++ web/BookDatabase/location.php
@@ -4,11 +4,13 @@ require "dbConnect.php";
 $db = get_db();
 
 $location = $_POST["location"];
-$locationName = $db->query("SELECT l.location FROM location l WHERE l.id = $location");
+//$locationName = $db->query("SELECT l.location FROM location l WHERE l.id = $location");
 $statement = $db->query("SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE l.id ='$location' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<h1>Below is a list of books located in $locationName </h1></br>";
+echo "<h1>Below is a list of books located in";
+echo $results["location"];
+echo "</h1></br>";
 echo "<ul class='list-group list-group-horizontal'>
         <li class='list-group-item'>Title</li>
         <li class='list-group-item'>Author</li>

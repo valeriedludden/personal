2c6ac3b5341a9631c48e8c943b9b5c5afb18ce1c
Valerie Ludden <valudden@gmail.com>
2020-05-23 22:48:51 -0600

    test2

--- web/BookDatabase/location.php
+++ web/BookDatabase/location.php
@@ -3,20 +3,25 @@ include "header.php";
 require "dbConnect.php";
 $db = get_db();
 
-$location = strtoupper($_POST["location"]);
+$location = $_POST["location"];
+$locationName = $db->query("SELECT location FROM location WHERE id = $location");
 $statement = $db->query("SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE l.id ='$location' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<h1>Below is a list of books located in  $location </h1>";
-
+echo "<h1>Below is a list of books located in $locationName </h1></br>";
+echo "<ul class='list-group list-group-horizontal'>
+        <li class='list-group-item'>Title</li>
+        <li class='list-group-item'>Author</li>
+        <li class='list-group-item'>Genre</li>
+    </ul>
+</div>"
 
 foreach ($results as $row)
 {
     echo "<div class='container'>
     <ul class='list-group list-group-horizontal'>
-        <li class='list-group-item'>" .$row['name']."</li>
         <li class='list-group-item'>" .$row['title']."</li>
-        <li class='list-group-item'>" .$row['location']."</li>
+        <li class='list-group-item'>" .$row['name']."</li>
         <li class='list-group-item'>" .$row['genre']."</li>
     </ul>
 </div>";

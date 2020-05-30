3e1be1f32e85901b93a3067a54826ef36418d881
Valerie Ludden <valudden@gmail.com>
2020-05-30 15:18:49 -0600

    added manage page

--- web/BookDatabase/newBook.php
+++ web/BookDatabase/newBook.php
@@ -8,21 +8,21 @@ $stuff = "Heelo Stuff";
 echo "Results";
 echo $stuff;
 
-$bId = $db->lastInsertId("author_id_seq");
-echo $bId;
+//$bId = $db->lastInsertId("author_id_seq");
+//echo $bId;
 
 //$query = "SELECT (b. id, b.title, a.name, l.location, g.genre) FROM book b, author a, location l, genre g WHERE b.id ='$bId' AND b.author = a.id AND b.location = l.id AND b.genre = g.id)";
 //$query = "SELECT (id, name) FROM author";
-$statement = $db->query("SELECT (id, location) FROM location");
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$st = $db->query("SELECT (id, location) FROM location");
+$rs = $st->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($results as $row) {
+foreach ($rs as $r) {
 //
 //    ?>
     <!--    <div>-->
 <!--    <h1>Stuff: --><?//= $stuff ?><!-- - </h1>-->
-    <p>ID: <?= $row['id'] ?> - </p>
-    <p>NAME: <?= $row['location'] ?></p><br>
+    <p>ID: <?= $r['id'] ?> - </p>
+    <p>NAME: <?= $r['location'] ?></p><br>
     <!--        <h1>Title: --><?//= $row['b.title'] ?><!--</h1><br>-->
     <!--        <h1>Author: --><?//= $row['a.name'] ?><!--</h1><br>-->
     <!--        <h1>Location: --><?//= $row['l.location'] ?><!--</h1><br>-->

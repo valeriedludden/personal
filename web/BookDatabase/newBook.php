b9e739998df8d6068dd720c4445577f3bfad67f4
Valerie Ludden <valudden@gmail.com>
2020-05-30 14:54:51 -0600

    added manage page

--- web/BookDatabase/newBook.php
+++ web/BookDatabase/newBook.php
@@ -9,6 +9,7 @@ echo "Results";
 echo $stuff;
 
 $bId = $db->lastInsertId("author_id_seq");
+echo $bId;
 
 //$query = "SELECT (b. id, b.title, a.name, l.location, g.genre) FROM book b, author a, location l, genre g WHERE b.id ='$bId' AND b.author = a.id AND b.location = l.id AND b.genre = g.id)";
 $query = "SELECT (id, name) FROM author WHERE id ='$bId'";

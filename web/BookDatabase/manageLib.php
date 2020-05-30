5fe79562bcd9bb9c678c017435536b1724c115b0
Valerie Ludden <valudden@gmail.com>
2020-05-30 15:12:56 -0600

    added manage page

--- web/BookDatabase/manageLib.php
+++ web/BookDatabase/manageLib.php
@@ -11,44 +11,44 @@ $addTitle = $_POST['add-title'];
 $addLocation = $_POST['add-location'];
 $addGenre = $_POST['add-genre'];
 
-try
-{
-    // Add the Scripture
-
-    // We do this by preparing the query with placeholder values
-
-    $statement = $db->prepare('INSERT INTO author(id, name) VALUES(DEFAULT, :addAuthor)');
-
-    // Now we bind the values to the placeholders. This does some nice things
-    // including sanitizing the input with regard to sql commands.
-    $statement->bindValue(':addAuthor', $addAuthor);
-//    $statement->bindValue(':aID', $chapter);
-//    $statement->bindValue(':verse', $verse);
-//    $statement->bindValue(':content', $content);
-
-
-    $statement->execute();
-
-    // get the new id
-//    $authorId = $db->lastInsertId("author_is_seq");
-// // Now go through each topic id in the list from the user's checkboxes
+//try
+//{
+//    // Add the Scripture
 //
-//        $statement = $db->prepare("INSERT INTO book(id, title, author, genre, location) VALUES(DEFAULT, ':title', :authorId, :genreId, :locationid)");
+//    // We do this by preparing the query with placeholder values
 //
-//        // Then, bind the values
-//        $statement->bindValue(':title', $addTitle);
-//        $statement->bindValue(':authorId', $authorId);
-//        $statement->bindValue(':genreId', $addGenre);
-//        $statement->bindValue(':locationId', $addLocation);
+//    $statement = $db->prepare('INSERT INTO author(id, name) VALUES(DEFAULT, :addAuthor)');
 //
-//        $statement->execute();
-}
-catch (Exception $ex)
-{
-    // Please be aware that you don't want to output the Exception message in
-    // a production environment
-    echo "Sorry there was a problem adding your book";
-}
+//    // Now we bind the values to the placeholders. This does some nice things
+//    // including sanitizing the input with regard to sql commands.
+//    $statement->bindValue(':addAuthor', $addAuthor);
+////    $statement->bindValue(':aID', $chapter);
+////    $statement->bindValue(':verse', $verse);
+////    $statement->bindValue(':content', $content);
+//
+//
+//    $statement->execute();
+//
+//    // get the new id
+////    $authorId = $db->lastInsertId("author_is_seq");
+//// // Now go through each topic id in the list from the user's checkboxes
+////
+////        $statement = $db->prepare("INSERT INTO book(id, title, author, genre, location) VALUES(DEFAULT, ':title', :authorId, :genreId, :locationid)");
+////
+////        // Then, bind the values
+////        $statement->bindValue(':title', $addTitle);
+////        $statement->bindValue(':authorId', $authorId);
+////        $statement->bindValue(':genreId', $addGenre);
+////        $statement->bindValue(':locationId', $addLocation);
+////
+////        $statement->execute();
+//}
+//catch (Exception $ex)
+//{
+//    // Please be aware that you don't want to output the Exception message in
+//    // a production environment
+//    echo "Sorry there was a problem adding your book";
+//}
 
 
 ?>

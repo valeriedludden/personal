6bbcf457ce1166c98f03f70ce1dbd29e22c92c5f
Valerie Ludden <valudden@gmail.com>
2020-05-30 14:40:00 -0600

    added manage page

--- web/BookDatabase/newBook.php
+++ web/BookDatabase/newBook.php
@@ -5,14 +5,15 @@ $db = get_db();
 
 $stuff = "Heelo Stuff";
 
-echo "Your new book is";
+echo "Results";
+echo $stuff;
 
 $bId = $db->lastInsertId("author_id_seq");
 
 //$query = "SELECT (b. id, b.title, a.name, l.location, g.genre) FROM book b, author a, location l, genre g WHERE b.id ='$bId' AND b.author = a.id AND b.location = l.id AND b.genre = g.id)";
 $query = "SELECT (id, name) FROM author WHERE id ='$bId'";
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo "Results";
+
 echo $results;
 foreach ($results as $row) {
 //

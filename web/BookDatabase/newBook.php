e4ead17485c831038a1726c218c3dc8206463a76
Valerie Ludden <valudden@gmail.com>
2020-05-30 19:19:11 -0600

    added manage page

--- web/BookDatabase/newBook.php
+++ web/BookDatabase/newBook.php
@@ -6,7 +6,7 @@ $db = get_db();
 $bId = $_SESSION["bookId"];
 $st = $db->query("SELECT b.id, b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE b.id = '$bId' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $rs = $st->fetchAll(PDO::FETCH_ASSOC);
-
+echo "The book id is " . $bId."<br>";
 echo "The book you picked is: ";
 if(count($rs) > 0){
 foreach ($rs as $r) {

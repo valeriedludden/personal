14def02078e2a6e1633ef4969b1cdf24f8632450
Valerie Ludden <valudden@gmail.com>
2020-05-30 19:09:23 -0600

    added manage page

--- web/BookDatabase/newBook.php
+++ web/BookDatabase/newBook.php
@@ -2,8 +2,8 @@
 include "header.php";
 require "dbConnect.php";
 $db = get_db();
-$bId = $db->lastInsertId("book_id_seq");
-//$bId = );
+//$bId = $db->lastInsertId("book_id_seq");
+$bId = 150;
 $st = $db->query("SELECT b.id, b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE b.id = '$bId' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $rs = $st->fetchAll(PDO::FETCH_ASSOC);
 

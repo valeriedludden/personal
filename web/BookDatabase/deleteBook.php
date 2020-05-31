d9e24801a465acf70b687ee294dab598833f7013
Valerie Ludden <valudden@gmail.com>
2020-05-30 22:52:33 -0600

    added manage page

--- web/BookDatabase/deleteBook.php
+++ web/BookDatabase/deleteBook.php
@@ -2,21 +2,21 @@
 include "header.php";
 require "dbConnect.php";
 $db = get_db();
-if(!isset($_POST['delete-title'])){
-?>
-<div class="deleteContainer">
-    <div class="manTitle "><h1>Delete a book from the library </h1></div>
-    <div>
-<!--        <form method="POST" action="tempDelete.php">-->
-        <form method="POST">
-            <label for="delete-title">Title of Book to Delete:</label>
-            <input type="text" id="delete-title" name="delete-title" placeholder="Book Title"><br/>
-            <input type="submit" value="Delete Book"> <br/>
-        </form>
+if (!isset($_POST['delete-title'])) {
+    ?>
+    <div class="deleteContainer">
+        <div class="manTitle "><h1>Delete a book from the library </h1></div>
+        <div>
+            <!--        <form method="POST" action="tempDelete.php">-->
+            <form method="POST">
+                <label for="delete-title">Title of Book to Delete:</label>
+                <input type="text" id="delete-title" name="delete-title" placeholder="Book Title"><br/>
+                <input type="submit" value="Delete Book"> <br/>
+            </form>
+        </div>
     </div>
-</div>
 
-<?php
+    <?php
 }
 $deleteTitle = strtoupper($_POST["delete-title"]);
 $statement = $db->query("SELECT b.id, b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE b.title ='$deleteTitle' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
@@ -27,33 +27,22 @@ if (count($results) > 0) {
     foreach ($results as $row) {
         ?>
         <div class='container'>
-            <ul class='list-group list-group-horizontal'>
-                <li class='list-group-item'><?= $row['title'] ?></li>
-                <li class='list-group-item'><?= $row['name'] ?></li>
-                <li class='list-group-item'><?= $row['location'] ?></li>
-                <li class='list-group-item'><?= $row['genre'] ?></li>
-            </ul>
+            <p>Title: <?= $row['title'] ?></p>
+            <p> Author: <?= $row['name'] ?></p>
+            <p>Genre: <?= $row['genre'] ?></p>
         </div>
         <form method="POST" action="tempDelete.php">
-            <input type="hidden" name="delete-book" value="<?=$row['id']?>">
+            <input type="hidden" name="delete-book" value="<?= $row['id'] ?>">
             <input type="submit" value="Yes, delete this book">
         </form>
-
-
-        <?php
+<?php
     }
     unset($_POST["delete-title"]);
-}
-else if(count($results) < 1 && isset($_POST["delete-title"])) {
+} else if (count($results) < 1 && isset($_POST["delete-title"])) {
     echo "I am sorry, there is no book with that title in this library";
 }
 
-
-echo ''
 ?>
-<div class="container">
-
-</div>
 </body>
 </html>
 

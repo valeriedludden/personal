f86906064eaf693728ec1e3739c65874f0520330
Valerie Ludden <valudden@gmail.com>
2020-05-30 21:45:57 -0600

    added manage page

--- web/BookDatabase/tempDelete.php
+++ web/BookDatabase/tempDelete.php
@@ -2,8 +2,8 @@
 include "header.php";
 require "dbConnect.php";
 $db = get_db();
-$book = strtoupper($_POST["delete-title"]);
-$statement = $db->query("SELECT b.id, b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE title ='$book' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
+$deleteTitle = strtoupper($_POST["delete-title"]);
+$statement = $db->query("SELECT b.id, b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE title ='$deleteTitle' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 if (count($results) > 0) {

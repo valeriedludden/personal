730dbc7394e21f86d19bd998927ae98ce97b49aa
Valerie Ludden <valudden@gmail.com>
2020-05-30 21:39:35 -0600

    added manage page

--- web/BookDatabase/deleteBook.php
+++ web/BookDatabase/deleteBook.php
@@ -7,15 +7,16 @@ $db = get_db();
 <div class="deleteContainer">
     <div class="manTitle "><h1>Delete a book from the library </h1></div>
     <div>
+<!--        <form method="POST" action="tempDelete.php">-->
         <form method="POST">
-            <label class="addTitle" for="delete-title">Title of Book to Delete:</label>
-            <input type="text" id="add-title" name="delete-title" placeholder="Book Title"><br/>
+            <label for="delete-title">Title of Book to Delete:</label>
+            <input type="text" id="delete-title" name="delete-title" placeholder="Book Title"><br/>
             <input type="submit" value="Delete Book"> <br/>
         </form>
     </div>
 </div>
 
-<?php $book = strtoupper($_POST["book"]);
+<?php $book = strtoupper($_POST["delete-title"]);
 $statement = $db->query("SELECT b.id, b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE title ='$book' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 

ddac9ed710b0b9ebe98875d382c08be088e85986
Valerie Ludden <valudden@gmail.com>
2020-05-30 19:10:46 -0600

    added manage page

--- web/BookDatabase/newBook.php
+++ web/BookDatabase/newBook.php
@@ -3,7 +3,7 @@ include "header.php";
 require "dbConnect.php";
 $db = get_db();
 //$bId = $db->lastInsertId("book_id_seq");
-$bId = 150;
+$bId = 300;
 $st = $db->query("SELECT b.id, b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE b.id = '$bId' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $rs = $st->fetchAll(PDO::FETCH_ASSOC);
 
@@ -14,8 +14,8 @@ foreach ($rs as $r) {
 
     <p>Title: <?= $r['title'] ?> - </p>
     <p> *Author: <?= $r['name'] ?></p>
-    <p> *Genre: <?= $r['genre'] ?></p><br>
-    <p> *Location: <?= $r['location'] ?></p><br>
+    <p> *Genre: <?= $r['genre'] ?></p>
+    <p> *Location: <?= $r['location'] ?></p>
     <?php
 }}
 else{

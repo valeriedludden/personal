b5afa004853033fdfa15caaaa99c1119c29cf8c5
Valerie Ludden <valudden@gmail.com>
2020-06-06 22:28:42 -0600

    final clean up

--- web/BookDatabase/genre.php
+++ web/BookDatabase/genre.php
@@ -15,7 +15,7 @@ foreach ($genId as $g) {
     $gId = $g["id"];
     echo "Genre 3". $gId;
 }
-$statement = $db->query("SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE b.genre ='$gId' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
+$statement = $db->query("SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE g.genre ='$genre' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<h1>Below is a list of books in the " .  $results['genre']." genre</h1></br>";

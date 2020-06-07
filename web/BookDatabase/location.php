d4146a511b2cc251a192193d7519fc5a732ef755
Valerie Ludden <valudden@gmail.com>
2020-06-06 23:24:25 -0600

    final clean up

--- web/BookDatabase/location.php
+++ web/BookDatabase/location.php
@@ -4,17 +4,17 @@ require "dbConnect.php";
 $db = get_db();
 
 $location = $_POST["location"];
-//$locationName = $db->query("SELECT l.location FROM location l WHERE l.id = $location");
+
 $statement = $db->query("SELECT b.title, a.name, l.location, l.id, g.genre FROM book b, author a, location l, genre g WHERE l.location ='$location' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-$locs = ['Den Bookcase Left','Den Bookcase Right', 'Barrister Bookcase West', 'Barrister Bookcase East', 'Guest Bedroom Shelves', 'STE Michelle Shopping Bag'];
+//$locs = ['Den Bookcase Left','Den Bookcase Right', 'Barrister Bookcase West', 'Barrister Bookcase East', 'Guest Bedroom Shelves', 'STE Michelle Shopping Bag'];
 
-echo "<h1>Below is a list of books located in the $locs[$location]</h1></br>";
+echo "<h1>Below is a list of books located in the $location</h1></br>";
 echo "<b><div class='container'>
 <ul class='list-group list-group-horizontal'>
-        <li class='list-group-item g-one'>Title</li>
-        <li class='list-group-item g-two'>Author</li>
-        <li class='list-group-item g-three'>Genre</li>
+        <li class='list-group-item g-one title'>Title</li>
+        <li class='list-group-item g-two title'>Author</li>
+        <li class='list-group-item g-three title'>Genre</li>
     </ul>
 </div></b>";
 

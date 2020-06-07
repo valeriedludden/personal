954e11566efc2d46f1a85fca1969072b5978cfc6
Valerie Ludden <valudden@gmail.com>
2020-06-06 22:14:52 -0600

    final clean up

--- web/BookDatabase/genre.php
+++ web/BookDatabase/genre.php
@@ -4,7 +4,13 @@ require "dbConnect.php";
 $db = get_db();
 
 $genre = $_POST["genre"];
-$statement = $db->query("SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE g.genre ='$genre' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
+$query2 = "SELECT id, genre FROM genre WHERE genre = '$genre'";
+$genId = $db->query($query2);
+$gId = 0;
+foreach ($genId as $g) {
+    $gId = $g["id"];
+}
+$statement = $db->query("SELECT b.title, a.name, l.location, g.genre FROM book b, author a, location l, genre g WHERE b.genre ='$gId' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<h1>Below is a list of books in the " .  $results['genre']." genre</h1></br>";

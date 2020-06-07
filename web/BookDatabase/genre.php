bedc215c8b434f277c98cfc2e6230f9d30da5ae1
Valerie Ludden <valudden@gmail.com>
2020-06-06 22:53:56 -0600

    final clean up

--- web/BookDatabase/genre.php
+++ web/BookDatabase/genre.php
@@ -7,9 +7,9 @@ $genre = $_POST["genre"];
 $statement = $db->query("SELECT b.title, a.name, l.location, g.genre, g.id FROM book b, author a, location l, genre g WHERE g.genre ='$genre' AND b.author = a.id AND b.location = l.id AND b.genre = g.id");
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<h1>Below is a list of books in the " .  $results['genre']." genre</h1></br>";
-echo "<b><div class='resultContainer'>
-<ul class='list-group list-group-horizontal'>
+echo "<h1>Below is a list of books in the " .  $genre . " genre</h1></br>";
+echo "<b><div class='container'>
+<ul class='list-group list-group-horizontal title'>
         <li class='list-group-item g-one'>Title</li>
         <li class='list-group-item g-two'>Author</li>
         <li class='list-group-item g-three'>Location</li>
@@ -18,7 +18,7 @@ echo "<b><div class='resultContainer'>
 
 
 foreach ($results as $row) {
-    echo "<div class='resultContainer'>
+    echo "<div class='container'>
     <ul class='list-group list-group-horizontal'>
         <li class='list-group-item g-one'>" . $row['title'] . "</li>
         <li class='list-group-item g-two'>" . $row['name'] . "</li>
